6ace9f25d15b5b940405e351581981be5a3b1c6d
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2015-02-01 01:24:16 -0200

    Initial Commit

---
+++ iOS Simulator Location Updater/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iOS Simulator Location Updater
+//
+//  Created by Francesco Perrotti-Garcia on 2/1/15.
+//  Copyright (c) 2015 Francesco Perrotti-Garcia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iOS Simulator Location Updater/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iOS Simulator Location Updater
+//
+//  Created by Francesco Perrotti-Garcia on 2/1/15.
+//  Copyright (c) 2015 Francesco Perrotti-Garcia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
